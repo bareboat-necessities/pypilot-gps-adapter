0d71467387ec37d9132b2f8c9d3b7a9b444523bb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-18 11:46:00 -0400

    Use algorithms GPS math in adapter

--- src/pypilot_gps_adapter/gps_filter_math.hpp
+++ src/pypilot_gps_adapter/gps_filter_math.hpp
@@ -1,59 +1,16 @@
 #pragma once
 
-#include <cmath>
+#include <pypilot_algorithms/gps_math.hpp>
 
 namespace pypilot_gps_adapter {
 
-static const double earth_radius_m = 6378137.0;
-static const double earth_m_per_degree = earth_radius_m * 2.0 * 3.14159265358979323846 / 360.0;
-
-template<typename Real = double>
-struct GpsXY {
-    Real x_m;
-    Real y_m;
-};
-
-template<typename Real = double>
-struct GpsLatLon {
-    Real latitude_deg;
-    Real longitude_deg;
-};
-
-template<typename Real>
-inline Real wrap_180_degrees(Real value) {
-    while (value > Real(180)) value -= Real(360);
-    while (value <= Real(-180)) value += Real(360);
-    return value;
-}
-
-template<typename Real>
-inline GpsXY<Real> ll_to_xy(Real latitude_deg,
-                            Real longitude_deg,
-                            Real origin_latitude_deg,
-                            Real origin_longitude_deg) {
-    const Real cs = std::cos(origin_latitude_deg * Real(3.14159265358979323846 / 180.0));
-    const Real yc = latitude_deg - origin_latitude_deg;
-    const Real xc = wrap_180_degrees(longitude_deg - origin_longitude_deg);
-    GpsXY<Real> out;
-    out.x_m = Real(earth_m_per_degree) * xc * cs;
-    out.y_m = Real(earth_m_per_degree) * yc;
-    return out;
-}
-
-template<typename Real>
-inline GpsLatLon<Real> xy_to_ll(Real x_m,
-                                Real y_m,
-                                Real origin_latitude_deg,
-                                Real origin_longitude_deg) {
-    const Real cs = std::cos(origin_latitude_deg * Real(3.14159265358979323846 / 180.0));
-    GpsLatLon<Real> out;
-    out.latitude_deg = y_m / Real(earth_m_per_degree) + origin_latitude_deg;
-    out.longitude_deg = x_m / Real(earth_m_per_degree) / cs + origin_longitude_deg;
-    return out;
-}
-
-inline double meters_per_second_to_knots(double value) {
-    return value * 1.9438444924406048;
-}
+using pypilot_algorithms::gps::GpsLatLon;
+using pypilot_algorithms::gps::GpsXY;
+using pypilot_algorithms::gps::earth_m_per_degree;
+using pypilot_algorithms::gps::earth_radius_m;
+using pypilot_algorithms::gps::ll_to_xy;
+using pypilot_algorithms::gps::meters_per_second_to_knots;
+using pypilot_algorithms::gps::wrap_180_degrees;
+using pypilot_algorithms::gps::xy_to_ll;
 
 } // namespace pypilot_gps_adapter
