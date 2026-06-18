42e9e96be56c915d7ec7d2eb43cd6f4c41b6b478
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-18 10:31:39 -0400

    Add adapter umbrella header

---
+++ src/pypilot_gps_adapter.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "pypilot_gps_adapter/gps_fix.hpp"
+#include "pypilot_gps_adapter/gps_filter_math.hpp"
+#include "pypilot_gps_adapter/gps_filter.hpp"
+#include "pypilot_gps_adapter/gps_sensor_bridge.hpp"
+#include "pypilot_gps_adapter/gpsd_json.hpp"
